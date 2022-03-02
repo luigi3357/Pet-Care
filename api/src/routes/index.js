273445fd1ac20f86081f77fcf46839df6c6937b3
luigi3357 <grillete2@gmail.com>
2022-03-01 21:45:09 -0300

    register y login local back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,46 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs')
-const { User } = require('../db')
+const { User } = require('../db');
+const { verifyEmail, hash, create, search, compare } = require('../services/login');
 
 const router = Router();
+
+
+
+router.post("/register", async (req, res)=>{
+    let { email, password }=req.body
+   
+    try {
+        let verify = verifyEmail(email)
+        if(verify=== true && password.length >= 8){
+            let hasheador = await hash(password)
+            let result = await create(email, hasheador)
+            return res.status(200).send(result)
+        }
+        if(verify === false){
+          return res.status(404).send("Email invalido")
+        }
+    } catch (error) {
+        return res.status(404).send(error)
+    }
+  })
+  
+  router.post("/login",async (req, res)=>{
+   let { email, password }=req.body
+  try {
+    let user = await search(email)
+    if(user){
+        let check = await compare(password, user)
+        if (check === true){
+           return res.status(200).json(user.id)
+        }
+        if(check === false){
+            return res.status(404).send("Lo siento password incorrecta")
+        }
+    }   
+  } catch (error) {      
+    return res.status(404).send(error)
+  }
+  })
 
 module.exports = router;
