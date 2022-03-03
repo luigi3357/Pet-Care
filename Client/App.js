2bdd55d4ab8f3d577315b91cc19d6845e79fde2d
Darian Esquivel <darianesquivelf@gmail.com>
2022-03-03 10:49:45 -0300

    add landingPage

--- Client/App.js
+++ Client/App.js
@@ -1,28 +1,26 @@
-import React from 'react'
-import Login from './screens/Login/Login'
-import Register from './screens/Login/Register'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-
-
+import React from "react";
+import Login from "./screens/Login/Login";
+import Register from "./screens/Login/Register";
+import LandingPage from "./screens/LandingPage/LandingPage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
-function App(){
-  
-  return(
-    
-    <Stack.Navigator screenOptions={{headerShown:false}}>
-      <Stack.Screen name="Login" component={Login}/>
-      <Stack.Screen name="Register" component={Register}/>
+function App() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="LandingPage" component={LandingPage} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
     </Stack.Navigator>
-  )
+  );
 }
 
 export default () => {
   return (
     <NavigationContainer>
-      <App/>
+      <App />
     </NavigationContainer>
-  )
-}
+  );
+};
