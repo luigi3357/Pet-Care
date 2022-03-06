761083e4aa029305f686bcd84aa349830b386457
luigi3357 <luigi3579@gmail.com>
2022-03-06 10:43:04 +0100

    precarga del json fake

--- Client/redux/store.js
+++ Client/redux/store.js
@@ -4,4 +4,6 @@ import thunk from 'redux-thunk';
 import rootReducer from "./reducer";
 
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const Store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export default Store
--- api/index.js
+++ api/index.js
@@ -1,8 +1,32 @@
+const { user, post, feedback } = require('./data/petCareUsersFake.js');
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { User } = require('./src/db');
+const { createPost, createReview } = require('./data/funcionesPreCarga.js');
 
 
-conn.sync({ force: true }).then(() => {
+
+conn.sync({ force: true }).then(async () => {
+
+  try {    
+    const users = await user.forEach(element => {
+      User.findOrCreate({
+      where:{ 
+        email:element.email,
+        name:element.name,
+        last_name:element.last_name,
+        password:element.password,
+      }
+      })
+    });     
+           
+    const posts =  post.map(e=>createPost(e)); 
+    
+    const reviews = feedback.map(el=>createReview(el))
+
+  } catch (error) {
+    console.log(error)
+  }
   server.listen(3001, () => {
     console.log('%s listening at 3001'); 
   });
