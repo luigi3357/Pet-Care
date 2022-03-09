91b43af6ba43df56797481cade8dcf8210191c9e
julio-hdz <jchj1793@gmail.com>
2022-03-09 16:36:06 -0300

    eliminando review mocks de HomeScreen. Agregando estilo a BottomPlus

--- Client/screens/Home/Componentes/HomeScreen.js
+++ Client/screens/Home/Componentes/HomeScreen.js
@@ -55,34 +55,3 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
   },
 });
-
-//   var reviewsPostCard = [];
-
-//   for(let i =0; i<5; i++){
-//     let arrAux = [];
-//   for(let i =0; i<8;i++){
-
-//     let obj = {
-//       titulo : getRandomName(3),
-//       rating : Math.ceil(Math.random()*5),
-//       usuario: getRandomName(2)
-//      ,review : getRandomName(Math.ceil(Math.random()*17))
-//     }
-//     arrAux.push(obj);
-
-//   }
-//   reviewsPostCard.push(arrAux);
-// }
-
-// function getRandomName(q) {
-//   const loremIpsum =
-//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-//   var string = "";
-//   for (let i = 0; i < q; i++) {
-//     string +=
-//       loremIpsum.split(" ")[
-//         Math.floor(Math.random() * loremIpsum.split(" ").length)
-//       ] + " ";
-//   }
-//   return string.trim();
-// }
