51732a6583866b9114968e232795b55674e20002
julio-hdz <jchj1793@gmail.com>
2022-03-10 09:21:32 -0300

    styling

--- Client/Store/Actions/index.js
+++ Client/Store/Actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import ACTION_TYPES from './ActionTypes.js';
 import {fetchData, fetchSuccess, fetchError} from './ApiAction';
 
-const localhost ='192.168.0.39' ; //'192.168.0.11'
+const localhost ='192.168.100.8' ; //'192.168.0.11'
 
 
 
--- Client/screens/Home/Componentes/ReviewCard.js
+++ Client/screens/Home/Componentes/ReviewCard.js
@@ -6,18 +6,20 @@ export default function ReviewCard({id,titulo, rating, usuario, message}) {
         <View style={styles.reviewContainer} >
             <Text style={styles.reviewTitle}>{titulo}</Text>
             <View style={{display:"flex", flexDirection:"row", textAlign:"center", alignItems:"center"}}>
-                <Image
-                    style= {{width:10, height:15}}
+                <View style={{display:"flex", flexDirection:"column", alignItems:"flex-end"}}>
+                    <Image
+                        style= {{width:22, height:20,  }}
 
                   
 
                     source={require('../../../assets/Gold_Star.png')}
 
                 />
-                <Text style={styles.reviewRating}> {rating}</Text>
-            </View>
+                <Text style={styles.reviewRating}>{rating}</Text>
+                </View>
             <Text style={styles.reviewUsuario}>{usuario}</Text>
             <Text style={styles.message}>{message}</Text>
+            </View>
             </View>
     )
 }
@@ -34,9 +36,10 @@ const styles = StyleSheet.create({
         ,borderStyle : "solid"
         // ,margin : 2
         ,borderRadius : 3
-        ,width :200
+        ,width :270
         ,height : 80
         ,marginHorizontal: 10
+        ,padding: 5
     }
 
     
@@ -48,9 +51,7 @@ const styles = StyleSheet.create({
     }
     ,reviewRating : {
         fontWeight : "bold"
-        ,textShadowColor : "black"
-        ,textShadowOffset : {width : 1, height : 1}
-        ,textShadowRadius : 2
+        ,fontSize:14
         ,margin: 0
     }
     ,reviewUsuario : {
@@ -66,7 +67,7 @@ const styles = StyleSheet.create({
         overflow : "scroll"
         // ,maxWidth : 60
         // ,fontFamily : 'Times New Roman'
-        ,margin:10
+        ,marginHorizontal:15
     }
 })
 
