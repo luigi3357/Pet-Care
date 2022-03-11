b13b09b1fee72fe4b6fb9feb8c92f9a31c22be92
julio-hdz <jchj1793@gmail.com>
2022-03-11 19:15:52 -0300

    actualizando la precarga y añadiendo atributos al modelo user

--- Client/App.js
+++ Client/App.js
@@ -46,9 +46,9 @@ function App() {
       <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
       <Stack.Screen name="Register" component={Register} />
     <Stack.Screen name="NewPassword" component={NewPassword} />  */}
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="Payment" component={Payment} />
         <Stack.Screen name="Form" component={FormCard} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
   );
