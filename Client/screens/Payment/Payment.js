fef2f13fcbc08010447ac77fe11c6a703bef3810
Sebastian Guerra <js.guerra07@hotmail.com>
2022-03-14 12:28:19 -0500

    Css componente Payment

--- Client/screens/Payment/Payment.js
+++ Client/screens/Payment/Payment.js
@@ -1,6 +1,8 @@
 import * as Linking from "expo-linking";
 import React, { useEffect, useState } from "react";
 import {
+  ImageBackground,
+  KeyboardAvoidingView,
   StyleSheet,
   Text,
   TextInput,
@@ -9,6 +11,7 @@ import {
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { postPayment } from "../../Store/Actions/index.js";
+import bgCard from "../../assets/bgCard.jpeg";
 
 export default function Payment(navigation) {
   const dispatch = useDispatch();
@@ -33,103 +36,111 @@ export default function Payment(navigation) {
 
   
   return (
-    <View style={styles.container}>
-      <Text /* style={styles.title} */>Payment Detail</Text>
-      <Text /* style={styles.title} */>Payment Detail</Text>
-      <Text /* style={styles.title} */>Payment Detail</Text>
-      <Text /* style={styles.title} */>Payment Detail</Text>
-      <Text /* style={styles.title} */>Payment Detail</Text>
-      <Text /* style={styles.title} */>Payment Detail</Text>
-      <Text /* style={styles.title} */>Payment Detail</Text>
+    <ImageBackground source={bgCard} style={[styles.bgCard, styles.container]}>
+    <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={20}>
+        
+        <Text style={{
+          justifyContent: "center",
+          margin: 10,
+          overflow: "hidden",
+          color: "white",
+          fontStyle: "italic",
+          fontSize: 16,
+          fontWeight: "bold",
+          backgroundColor: "#005e50",
+          padding: 10,
+          borderRadius: 10,
+        }} >DETALLE DE PAGO</Text>
 
-      <Text /* style={styles.title} */>CARER:</Text>
+        <Text  style={styles.title} >CUIDADOR:</Text>
 
+        <TextInput
+            type = "text"
+            name = "carer"
+            style = {styles.input}
+            placeholder = "Nombre de cuidador..."
+            value = {carer}
+            onChangeText = {(value) => onChangeCarer(value)}
+        />
+        <Text>{carer}</Text>
 
-      <TextInput
-        type="text"
-        name="carer"
-        style={styles.input}
-        placeholder="Carer..."
-        value={carer}
-        onChangeText={(value) => onChangeCarer(value)}
-      />
-      <Text>{carer}</Text>
+        <Text style={styles.title} >MONTO:</Text>
 
-      <Text /* style={styles.title} */>AMOUNT:</Text>
+        <TextInput
+            type = "text" 
+            keyboardType = "numeric" 
+            name = "amount"
+            style = {styles.input}
+            placeholder = "Valor del servicio..."
+            value = {amount}
+            onChangeText = {(value) => onChangeAmount(value)} />
+        <Text>${amount}</Text>   
+        
+        <TouchableOpacity type = "submit" title="payment" onPress={handlePayment}>  
+            <Text style={styles.button}>Ir a pagar</Text> 
+        </TouchableOpacity>  
 
-      <TextInput
-        type="text"
-        //keyboardType = "numeric"
-        name="amount"
-        style={styles.input}
-        placeholder="Amount..."
-        value={amount}
-        onChangeText={(value) => onChangeAmount(value)}
-      />
-      <Text>${amount}</Text>
-
-      <TouchableOpacity type="submit" title="payment" onPress={handlePayment}>
-        <Text style={styles.button}>Go to payment</Text>
-      </TouchableOpacity>
-    </View>
-  )
+    </KeyboardAvoidingView>
+    </ImageBackground>
+)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    backgroundColor: "#60D394",
-    borderRadius: 10,
-    borderWidth: 4,
-    borderColor: "#CCC",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 15,
-    minWidth: 97,
-    margin: 40,
-    // ,zIndex : 4
-  },
-  title: {
-    fontSize: 1.5,
-    // fontFamily : "Helvetica",
-    fontWeight: "700",
-  },
-  rating: {
-    fontSize: 1.2,
-    color: "gold",
-    // fontFamily : "Courier New",
-    fontWeight: "bold",
-    textShadowColor: "#FFF",
-    textShadowOffset: { width: 1, height: 1 },
-  },
-  button: {
-    backgroundColor: "#8aF",
-    marginTop: 0.5,
-    color: "#FFF",
-    display: "flex",
-    justifyContent: "flex-end",
-
-    alignItems: "center",
-    // ,flex:1
-    padding: 4,
-  },
-  details: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-  },
-  detailsReviews: {
-    display: "flex",
-    flexDirection: "row",
-    //   ,width : 100
-    //   ,marginLeft : '2px solid green'
-  },
-  input: {
+container: {
+  display: "flex",
+  backgroundColor: "#00d2c6",
+  borderRadius: 10,
+  borderWidth: 2,
+  borderColor: "#CCC",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: 15,
+  minWidth: 97,
+  margin: 0,
+  // ,zIndex : 4
+},
+title: {
+  fontSize: 12,
+  fontFamily : "Helvetica",
+  fontWeight: "700",
+},
+button: {
+  backgroundColor: "#7C169E",
+  marginTop: 1,
+  color: "#FFF",
+  display: "flex",
+  alignSelf: "flex-end",
+  alignItems: "center",
+  padding: 11,
+  borderRadius: 10,
+  fontWeight: "bold",
+  overflow: "hidden",
+  borderBottomStartRadius: 30
+},
+detailsReviews: {
+  display: "flex",
+  flexDirection: "row",
+  //   ,width : 100
+  //   ,marginLeft : '2px solid green'
+},
+input: {
     borderWidth: 1,
-    borderColor: "#777",
+    borderColor: "white",
+    color: "grey",
     padding: 8,
     margin: 10,
     width: 200,
-  },
-});
+    borderRadius: 10,
+    backgroundColor: "white"
+},
+bgCard: {
+  flex: 1,
+  justifyContent: "center",
+  resizeMode: "contain",
+},
+text: {
+  color: "white",
+  fontSize: 16,
+}
+
+})
